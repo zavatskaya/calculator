4ec2b7750f197561fceddb154252f2bf29795f20
Liliya Zavatskaya <zavatskaya.liliya@icloud.com>
2018-12-24 10:34:52 +0600

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Liliya Zavatskaya on 12/24/18.
+//  Copyright © 2018 Liliya Zavatskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
